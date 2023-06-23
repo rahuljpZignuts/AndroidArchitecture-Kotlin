3b5f48e4cfde31b23d913928dbe8da28a254faf5
rahul-parmar <rahuljp@zignuts.com>
2023-06-23 16:46:11 +0530

    Leanback , Timber , AGP Upgrade to 8.0.2

--- app/src/main/java/com/rahul/kotlin/architecture/application/App.kt
+++ app/src/main/java/com/rahul/kotlin/architecture/application/App.kt
@@ -2,6 +2,18 @@ package com.rahul.kotlin.architecture.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        plantTimber()
+    }
+
+    private fun plantTimber() {
+        if (Config.environment == Config.Environment.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
